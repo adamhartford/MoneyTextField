c1125b41835108f09e36ccdeb431b11b0b29beff
Adam Hartford <adam@adamhartford.com>
2015-05-23 20:37:42 -0400

    Initial Commit

---
+++ MoneyTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTextField
+//
+//  Created by Adam Hartford on 5/23/15.
+//  Copyright (c) 2015 Adam Hartford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
